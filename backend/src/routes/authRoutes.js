46915495714acd23bab621a46fcb57e89a276ede
William Sun <williamsun0908@Williams-MacBook-Air.local>
2025-06-22 15:44:29 -0400

    add authRoutes URLs

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -9,11 +9,11 @@ r.get("/google",
 
 r.get("/google/callback",
   passport.authenticate("google", { failureRedirect: "/auth/login-failed" }),
-  (_req, res) => res.redirect("http://localhost:5001")
+  (_req, res) => res.redirect("https://thoughtsoverflow.com")
 );
 
 r.get("/login-failed", (_req, res) => res.status(401).send("Login failed"));
-r.get("/logout", (req, res) => req.logout(() => res.redirect("http://localhost:5001")));
+r.get("/logout", (req, res) => req.logout(() => res.redirect("https://thoughtsoverflow.com")));
 r.get("/current-user", (req, res) => res.json(req.user || null));
 
 export default r;
